494ad93ff09033b150aeb5d5bd9808967166d4a3
mszalbach <4-bar-killa@web.de>
2016-08-17 15:05:38 +0200

    - changed webpack.config to be separated by npm step

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
-var path = require( 'path' );
-var webpack = require( 'webpack' );
-var ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var merge = require('webpack-merge');
+const path = require( 'path' );
+const webpack = require( 'webpack' );
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+const merge = require( 'webpack-merge' );
+const validate = require('webpack-validator');
+
 
 const PATHS = {
-    app: path.join(__dirname, 'src/main/js'),
-    build: path.join(__dirname, 'build')
+    app: path.join( __dirname, 'src/main/js' ),
+    build: path.join( __dirname, 'build' )
 };
 
 
-module.exports = {
-    entry: './src/main/js/App.jsx',
+const common = {
+    entry: {
+        app: PATHS.app
+    },
     devtool: 'source-map',
     output: {
         path: PATHS.build,
-        filename: 'bundle.js'
+        filename: '[name].js'
     },
 
     resolve: {
@@ -32,8 +36,22 @@ module.exports = {
 
     plugins: [
         new ExtractTextPlugin( 'bundle.css' ),
-        new HtmlWebpackPlugin({
+        new HtmlWebpackPlugin( {
             template: './src/main/js/index.html'
-        })
+        } )
     ]
 };
+
+
+var config;
+
+// Detect how npm is run and branch based on that
+switch ( process.env.npm_lifecycle_event ) {
+    case 'build':
+        config = merge( common, {} );
+        break;
+    default:
+        config = merge( common, {} );
+}
+
+module.exports = validate(config);
