8208c2815e886acac4b399b262626a8ef73c20f7
mszalbach <4-bar-killa@web.de>
2016-08-17 19:06:55 +0200

    - seperated app code from vendor code via chunks

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const path = require( 'path' );
-const webpack = require( 'webpack' );
-const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
-const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
-const merge = require( 'webpack-merge' );
-const validate = require( 'webpack-validator' );
-
-const parts = require( './libs/parts' );
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
+const pkg = require("./package.json");
 
 
 const PATHS = {
-    app: path.join( __dirname, 'src/main/js' ),
-    build: path.join( __dirname, 'build' )
+    app: path.join(__dirname, 'src/main/js'),
+    build: path.join(__dirname, 'build')
 };
 
 
 const common = {
     entry: {
-        app: PATHS.app
+        app: PATHS.app,
+        vendor : Object.keys(pkg.dependencies)
     },
     devtool: 'source-map',
     output: {
@@ -32,15 +32,15 @@ const common = {
     module: {
         loaders: [
             {test: /\.js[x]?$/, exclude: /node_modules/, loader: 'babel-loader'},
-            {test: /\.css$/, loader: ExtractTextPlugin.extract( 'style-loader', 'css-loader' )}
+            {test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader')}
         ]
     },
 
     plugins: [
-        new ExtractTextPlugin( 'bundle.css' ),
-        new HtmlWebpackPlugin( {
+        new ExtractTextPlugin('bundle.css'),
+        new HtmlWebpackPlugin({
             template: './src/main/js/index.html'
-        } )
+        })
     ]
 };
 
@@ -48,31 +48,30 @@ const common = {
 var config;
 
 // Detect how npm is run and branch based on that
-switch ( process.env.npm_lifecycle_event ) {
+switch (process.env.npm_lifecycle_event) {
     case 'build':
-        config = merge( common, {
+        config = merge(common, {
             devtool: 'cheap-source-map',
-            plugins: [new webpack.DefinePlugin( {
-                'process.env.NODE_ENV': JSON.stringify( 'production' )
+            plugins: [new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify('production')
 
-            } ),
-                      new webpack.optimize.UglifyJsPlugin( {
-                          compress: {
-                              warnings: false
-                          }
-                      } )
+            }),
+                new webpack.optimize.UglifyJsPlugin({
+                    compress: {
+                        warnings: false
+                    }
+                }),
+                new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.js")
             ]
 
-        } );
+        });
         break;
     default:
-        config = merge(
-                common, {
-                    devtool: 'eval-source-map'
-                },
-                parts.devServer( {host: process.env.HOST, port: process.env.PORT} )
+        config = merge(common, {
+                devtool: 'eval-source-map'
+            }
         );
 
 }
 
-module.exports = validate( config );
+module.exports = validate(config);
